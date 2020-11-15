e61aefc317837e666c00037b9c1f24feb1d2d40b
unknown <ClaudiaEsp>
2020-11-15 21:32:10 +0100

    starting repository

---
+++ fleximaus/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+fleximaus: 
+
+A python module to analyze behavior and silicone probes in mouse performing 
+in a cognitive flexibility task
+
+"""
+
+#-------------------------------------------------------------------------
+# Global variables
+#-------------------------------------------------------------------------
+__version__ = 'v0.1alpha'
+__author__ = 'Claudia Espinoza'
+__email__ = 'claumespinoza at gmail.com'
+
+
+#-------------------------------------------------------------------------
+# Imports
+#-------------------------------------------------------------------------
+
